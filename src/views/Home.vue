c6b9b57ed6bf5af6b9c58a26c51d820a95048971
Md Nazmul Hasan <farhapartex@gmail.com>
2020-02-05 04:44:05 +0600

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Component, Prop, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+@Component({
+  name: "Home",
+  components: {}
+})
+export default class Home extends Vue {
+  // dummy
 }
 </script>
